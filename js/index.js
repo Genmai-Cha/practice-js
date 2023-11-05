f41807e05c0a2e75590da7079fd8124610f1daf0
Genmai <hotaru103bsb@gmail.com>
2023-11-05 21:46:20 +0900

    add: アコーディオン

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
-console.log('ほげ');
+// 質問部分が配列で取得できる
+const questions = Array.from(document.getElementsByClassName("accordion-question"));
+
+questions.map((question) => {
+    question.addEventListener('click', () => {
+        // 問題の同じ階層にある次のnode(回答)を取得する
+        question.nextElementSibling.classList.toggle('accordion-answer-display');
+
+        if (question.querySelector('span').classList.contains('triangle-close')) {
+            question.querySelector('span').classList.remove('triangle-close');
+            question.querySelector('span').classList.add('triangle-open');
+        } else {
+            question.querySelector('span').classList.remove('triangle-open'); 
+            question.querySelector('span').classList.add('triangle-close');
+        }
+    })    
+})
