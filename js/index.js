42974384015c56714abebd799ccc5c35217c170f
Genmai <hotaru103bsb@gmail.com>
2023-11-12 02:06:30 +0900

    add: タブ

--- js/index.js
+++ js/index.js
@@ -15,3 +15,28 @@ questions.map((question) => {
         }
     })    
 })
+
+// タブメニュー
+// 参考: https://zenn.dev/kiriyama/articles/b42b6b0761b08d
+const tabMenu = document.querySelectorAll('.tab-menu > li');
+const tabBody = document.getElementsByClassName("tab-content")
+
+tabMenu.forEach((menu) => {
+    menu.addEventListener('click', (e) => {
+        // 1. is-active classをもっているnodeを取得して、 一旦classを外す 
+        // アクティブになっているのは常に1つだけの想定なので、0を指定する
+        document.getElementsByClassName("is-active")[0].classList.remove("is-active");
+        document.getElementsByClassName("is-tab-active")[0].classList.remove("is-tab-active");
+
+        // 2. クリックしたところのtabとコンテンツをアクティブにする
+        // 常時1つはアクティブにしたいため、toggle()は使わない
+        e.target.classList.add("is-active");
+
+        for (let index = 0; index < tabBody.length; index++) {
+            // クリックしたtabのdata-tabとコンテンツのidが一緒の場合は、is-tab-activeを追加する
+            if (e.target.dataset.tab === tabBody[index].id ) {
+                tabBody[index].classList.add("is-tab-active");
+            }
+        }
+    });
+});
